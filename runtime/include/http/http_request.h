5e937fc20496b639a995b624b9de5ce25e6fa124
Sean McBride <spmcbride1201@gmail.com>
2020-03-16 20:07:04 -0400

    chore: decompose http

---
+++ runtime/include/http/http_request.h
@@ -0,0 +1,30 @@
+#ifndef SFRT_HTTP_REQUEST_H
+#define SFRT_HTTP_REQUEST_H
+
+#include <types.h>
+
+/* all in-memory ptrs.. don't mess around with that! */
+struct http_header {
+	char *key;
+	char *value;
+};
+
+struct http_request {
+	struct http_header headers[HTTP_HEADERS_MAX];
+	int                header_count;
+	char *             body;
+	int                body_length;
+	int 			   body_read_length; // How far we've read
+	// additional for http-parser
+	int last_was_value; // http-parser flag used to help the http-parser callbacks differentiate between header fields and values to know when to allocate a new header
+	int header_end;     // boolean flag set when header processing is complete
+	int message_begin;  // boolean flag set when body processing begins
+	int message_end;    // boolean flag set when body processing is complete
+};
+
+/***************************************************
+ * General HTTP Request Functions                  *
+ **************************************************/
+int http_request__get_body(struct http_request *http_request, char **body);
+
+#endif /* SFRT_HTTP_HEADER_H */
