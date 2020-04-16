6478ff9b84fdbd86de8107dd4c92ac521a6c5533
Sean McBride <spmcbride1201@gmail.com>
2020-04-16 10:42:24 -0400

    feat: WIP ps

---
+++ runtime/include/sandbox_request_scheduler_ps.h
@@ -0,0 +1,8 @@
+#ifndef SFRT_SANDBOX_REQUEST_SCHEDULER_PS_H
+#define SFRT_SANDBOX_REQUEST_SCHEDULER_PS_H
+
+#include <sandbox_request_scheduler.h>
+
+void sandbox_request_scheduler_ps_initialize();
+
+#endif /* SFRT_SANDBOX_REQUEST_SCHEDULER_PS_H */
---
+++ runtime/src/sandbox_request_scheduler_ps.c
@@ -0,0 +1,44 @@
+#include <sandbox_request_scheduler.h>
+#include "priority_queue.h"
+
+// Local State
+static struct priority_queue sandbox_request_scheduler_ps;
+
+/**
+ * Pushes a sandbox request to the global deque
+ * @param sandbox_request
+ * @returns pointer to request if added. NULL otherwise
+ **/
+static sandbox_request_t *
+sandbox_request_scheduler_ps_add(void *sandbox_request_raw)
+{
+	// TODO
+}
+
+/**
+ *
+ * @returns A Sandbox Request or NULL
+ **/
+static sandbox_request_t *
+sandbox_request_scheduler_ps_remove(void)
+{
+	// TODO
+}
+
+/**
+ *
+ **/
+void
+sandbox_request_scheduler_ps_initialize()
+{
+	// Initialize local state
+
+	// TODO
+
+
+	// Register Function Pointers for Abstract Scheduling API
+	sandbox_request_scheduler_config_t config = { .add    = sandbox_request_scheduler_ps_add,
+		                                      .remove = sandbox_request_scheduler_ps_remove };
+
+	sandbox_request_scheduler_initialize(&config);
+}
