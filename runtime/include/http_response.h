c291b049c884db8d2e4b6f1ce9bfc1f20f9a4004
Sean McBride <spmcbride1201@gmail.com>
2020-07-31 15:46:51 -0400

    fix: Properly close malformed requests

--- runtime/include/http_response.h
+++ runtime/include/http_response.h
@@ -11,6 +11,7 @@
 
 #define HTTP_RESPONSE_200_OK                    "HTTP/1.1 200 OK\r\n"
 #define HTTP_RESPONSE_504_SERVICE_UNAVAILABLE   "HTTP/1.1 504 Service Unavailable\r\n\r\n"
+#define HTTP_RESPONSE_400_BAD_REQUEST           "HTTP/1.1 400 Bad Request\r\n\r\n"
 #define HTTP_RESPONSE_CONTENT_LENGTH            "Content-Length: "
 #define HTTP_RESPONSE_CONTENT_LENGTH_TERMINATOR "\r\n\r\n" /* content body follows this */
 #define HTTP_RESPONSE_CONTENT_TYPE              "Content-Type: "
