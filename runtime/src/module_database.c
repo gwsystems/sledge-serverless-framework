3c7e0625485f7b13b9f0727751d6db28c84cd383
Sean McBride <spmcbride1201@gmail.com>
2020-08-23 14:36:13 -0400

    refactor: cleanup module DB

--- runtime/src/module_database.c
+++ runtime/src/module_database.c
@@ -4,8 +4,8 @@
  * Module Database *
  ******************/
 
-struct module *module_database[MODULE_MAX_MODULE_COUNT] = { NULL }; /* In-memory representation of all active modules */
-int            module_database_free_offset              = 0;        /* First free in module */
+struct module *module_database[MODULE_MAX_MODULE_COUNT] = { NULL };
+size_t         module_database_count                    = 0;
 
 /**
  * Given a name, find the associated module
@@ -15,8 +15,7 @@ int            module_database_free_offset              = 0;        /* First fre
 struct module *
 module_database_find_by_name(char *name)
 {
-	int f = module_database_free_offset;
-	for (int i = 0; i < f; i++) {
+	for (size_t i = 0; i < module_database_count; i++) {
 		assert(module_database[i]);
 		if (strcmp(module_database[i]->name, name) == 0) return module_database[i];
 	}
@@ -31,8 +30,7 @@ module_database_find_by_name(char *name)
 struct module *
 module_database_find_by_socket_descriptor(int socket_descriptor)
 {
-	int f = module_database_free_offset;
-	for (int i = 0; i < f; i++) {
+	for (size_t i = 0; i < module_database_count; i++) {
 		assert(module_database[i]);
 		if (module_database[i]->socket_descriptor == socket_descriptor) return module_database[i];
 	}
