f7d68b14203daa8f8f3760aec013a7275b8f140e
Sean McBride <spmcbride1201@gmail.com>
2020-08-23 14:36:13 -0400

    refactor: cleanup module DB

--- runtime/include/module_database.h
+++ runtime/include/module_database.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <errno.h>
+
 #include "debuglog.h"
 #include "module.h"
 
@@ -7,29 +9,28 @@ struct module *module_database_find_by_name(char *name);
 struct module *module_database_find_by_socket_descriptor(int socket_descriptor);
 
 extern struct module *module_database[];
-extern int            module_database_free_offset;
+extern size_t         module_database_count;
 
 /**
  * Adds a module to the in-memory module DB
  * @param module module to add
- * @return 0 on success. Aborts program on failure
+ * @return 0 on success. -ENOSPC when full
  */
 static inline int
 module_database_add(struct module *module)
 {
-	assert(module_database_free_offset >= 0);
-	assert(module_database_free_offset <= MODULE_MAX_MODULE_COUNT);
+	assert(module_database_count <= MODULE_MAX_MODULE_COUNT);
 
 	int rc;
 
-	if (module_database_free_offset == MODULE_MAX_MODULE_COUNT) goto err_no_space;
-	module_database[module_database_free_offset++] = module;
+	if (module_database_count == MODULE_MAX_MODULE_COUNT) goto err_no_space;
+	module_database[module_database_count++] = module;
 
 	rc = 0;
 done:
 	return rc;
 err_no_space:
 	debuglog("Cannot add module. Database is full.\n");
-	rc = -1;
+	rc = -ENOSPC;
 	goto done;
 }
