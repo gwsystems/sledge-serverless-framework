74a4397bbc9d06c7dbcfc17a34c7eba91d77f8c8
Sean McBride <spmcbride1201@gmail.com>
2020-10-20 14:52:22 -0400

    chore: Update preemption with new keys

--- runtime/experiments/preemption/debug.sh
+++ runtime/experiments/preemption/debug.sh
@@ -4,16 +4,16 @@
 # This allows debugging outside of the Docker container
 # Also disables pagination and stopping on SIGUSR1
 
-declare project_path="$(
-  cd "$(dirname "$1")/../.."
-  pwd
-)"
-echo $project_path
-cd ../../bin
-export LD_LIBRARY_PATH="$(pwd):$LD_LIBRARY_PATH"
+experiment_directory=$(pwd)
+project_directory=$(cd ../.. && pwd)
+binary_directory=$(cd "$project_directory"/bin && pwd)
+
+export LD_LIBRARY_PATH="$binary_directory:$LD_LIBRARY_PATH"
+export PATH="$binary_directory:$PATH"
+
 gdb --eval-command="handle SIGUSR1 nostop" \
+  --eval-command="handle SIGPIPE nostop" \
   --eval-command="set pagination off" \
-  --eval-command="set substitute-path /sledge/runtime $project_path" \
-  --eval-command="run ../tests/mixed_preemption/test_mixed_preemption.json" \
-  ./sledgert
-cd ../../tests
+  --eval-command="set substitute-path /sledge/runtime $project_directory" \
+  --eval-command="run $experiment_directory/spec.json" \
+  sledgert
