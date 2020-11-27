b61da62c2b7dbb5d5219bb7cec22f67b50c6fc5a
Sean McBride <spmcbride1201@gmail.com>
2020-11-27 16:23:04 -0500

    chore: fix missing include

--- runtime/include/panic.h
+++ runtime/include/panic.h
@@ -5,6 +5,7 @@
 #include <sched.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define panic(fmt, ...)                                                                                           \
 	{                                                                                                         \
