66482d012c7faf2c0ef3b4f3978ba14ae6ad265e
Sean McBride <spmcbride1201@gmail.com>
2021-04-07 21:20:14 -0400

    chore: Explicitly install clang-format-11 to avoid noisy diffs

--- install_llvm.sh
+++ install_llvm.sh
@@ -8,22 +8,25 @@ echo "Installing LLVM $LLVM_VERSION"
 
 # Script Installs clang, lldb, lld, and clangd
 if [[ "$LLVM_VERSION" -gt 8 ]]; then
-  curl --proto '=https' --tlsv1.2 -sSf https://apt.llvm.org/llvm.sh | bash -s -- "$LLVM_VERSION"
+	curl --proto '=https' --tlsv1.2 -sSf https://apt.llvm.org/llvm.sh | bash -s -- "$LLVM_VERSION"
 else
-  apt-get install -y --no-install-recommends \
-    "clang-$LLVM_VERSION" \
-    "lldb-$LLVM_VERSION" \
-    "lld-$LLVM_VERSION" \
-    "clangd-$LLVM_VERSION"
+	apt-get install -y --no-install-recommends \
+		"clang-$LLVM_VERSION" \
+		"lldb-$LLVM_VERSION" \
+		"lld-$LLVM_VERSION" \
+		"clangd-$LLVM_VERSION"
 fi
 
 apt-get install -y --no-install-recommends \
-  "libc++-$LLVM_VERSION-dev" \
-  "libc++abi-$LLVM_VERSION-dev" \
-  "libc++1-$LLVM_VERSION" \
-  "clang-format-$LLVM_VERSION"
+	"libc++-$LLVM_VERSION-dev" \
+	"libc++abi-$LLVM_VERSION-dev" \
+	"libc++1-$LLVM_VERSION"
+
+# Explicitly use clang-format-11 because of changes between 10 and 11
+apt-get install -y --no-install-recommends \
+	"clang-format-11"
 
 update-alternatives --install /usr/bin/clang clang "/usr/bin/clang-$LLVM_VERSION" 100
 update-alternatives --install /usr/bin/clang++ clang++ "/usr/bin/clang++-$LLVM_VERSION" 100
 update-alternatives --install /usr/bin/llvm-config llvm-config "/usr/bin/llvm-config-$LLVM_VERSION" 100
-update-alternatives --install /usr/bin/clang-format clang-format "/usr/bin/clang-format-$LLVM_VERSION" 100
+update-alternatives --install /usr/bin/clang-format clang-format "/usr/bin/clang-format-11" 100
