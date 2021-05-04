b9d832530590e5c6113c1a25484a493d926f8aab
Sean McBride <spmcbride1201@gmail.com>
2021-05-04 23:04:29 +0000

    chore: Refactor generic thread

--- runtime/include/generic_thread.h
+++ runtime/include/generic_thread.h
@@ -2,29 +2,9 @@
 
 #include <stdint.h>
 
-#include "arch/getcycles.h"
-#include "debuglog.h"
-#include "runtime.h"
-
 extern __thread uint64_t generic_thread_lock_duration;
 extern __thread uint64_t generic_thread_lock_longest;
 extern __thread uint64_t generic_thread_start_timestamp;
 
-/**
- * Reports lock contention
- */
-static inline void
-generic_thread_dump_lock_overhead()
-{
-#ifndef NDEBUG
-#ifdef LOG_LOCK_OVERHEAD
-	uint64_t duration = __getcycles() - generic_thread_start_timestamp;
-	debuglog("Locks consumed %lu / %lu cycles, or %f%%\n", generic_thread_lock_duration, duration,
-	         (double)generic_thread_lock_duration / duration * 100);
-	debuglog("Longest Held Lock was %lu cycles, or %f quantums\n", generic_thread_lock_longest,
-	         (double)generic_thread_lock_longest / ((uint64_t)runtime_processor_speed_MHz * runtime_quantum_us));
-#endif
-#endif
-}
-
+void generic_thread_dump_lock_overhead();
 void generic_thread_initialize();
--- runtime/src/generic_thread.c
+++ runtime/src/generic_thread.c
@@ -1,12 +1,32 @@
 #include <stdint.h>
 
+#include "generic_thread.h"
 #include "arch/getcycles.h"
+#include "debuglog.h"
+#include "runtime.h"
 
 /* Implemented by listener and workers */
 
 __thread uint64_t generic_thread_lock_duration   = 0;
 __thread uint64_t generic_thread_lock_longest    = 0;
 __thread uint64_t generic_thread_start_timestamp = 0;
+
+/**
+ * Reports lock contention
+ */
+void
+generic_thread_dump_lock_overhead()
+{
+#ifndef NDEBUG
+#ifdef LOG_LOCK_OVERHEAD
+	uint64_t duration = __getcycles() - generic_thread_start_timestamp;
+	debuglog("Locks consumed %lu / %lu cycles, or %f%%\n", generic_thread_lock_duration, duration,
+	         (double)generic_thread_lock_duration / duration * 100);
+	debuglog("Longest Held Lock was %lu cycles, or %f quantums\n", generic_thread_lock_longest,
+	         (double)generic_thread_lock_longest / ((uint64_t)runtime_processor_speed_MHz * runtime_quantum_us));
+#endif
+#endif
+}
 
 void
 generic_thread_initialize()
