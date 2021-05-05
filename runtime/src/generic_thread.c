f665c6ab1f7493d42f0eed500eee7d6e2e8f6ad8
Sean McBride <spmcbride1201@gmail.com>
2021-05-04 23:07:33 -0400

    refactor: generic thread

--- runtime/src/generic_thread.c
+++ runtime/src/generic_thread.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 
 #include "arch/getcycles.h"
+#include "debuglog.h"
 
 /* Implemented by listener and workers */
 
@@ -12,4 +13,19 @@ generic_thread_initialize()
 {
 	generic_thread_start_timestamp = __getcycles();
 	generic_thread_lock_duration   = 0;
+}
+
+/**
+ * Reports lock contention
+ */
+void
+generic_thread_dump_lock_overhead()
+{
+#ifndef NDEBUG
+#ifdef LOG_LOCK_OVERHEAD
+	uint64_t duration = __getcycles() - generic_thread_start_timestamp;
+	debuglog("Locks consumed %lu / %lu cycles, or %f%%\n", generic_thread_lock_duration, duration,
+	         (double)generic_thread_lock_duration / duration * 100);
+#endif
+#endif
 }
