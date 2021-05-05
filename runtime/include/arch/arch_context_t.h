8567da117b151c67df8e4fe6782c7d5a235f44b1
Sean McBride <spmcbride1201@gmail.com>
2021-05-05 19:50:06 +0000

    chore: decompose arch context t

---
+++ runtime/include/arch/arch_context_t.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <ucontext.h>
+
+#include "arch/reg_t.h"
+#include "arch/ureg_t.h"
+#include "arch/arch_context_variant_t.h"
+
+struct arch_context {
+	arch_context_variant_t variant;
+	reg_t                  regs[UREG_COUNT];
+	mcontext_t             mctx;
+	bool                   preemptable;
+};
