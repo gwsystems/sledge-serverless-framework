4e269aedf3435d2f4a0ca11a6bbbefd7e0482105
xiaosuGW <xiaosuGW@localhost>
2021-07-16 16:05:34 -0500

    update start.sh and test.sh

--- runtime/tests/start.sh
+++ runtime/tests/start.sh
@@ -1,10 +1,25 @@
+function usage {
+        echo "$0 [perf output file, chain_function_perf.log or single_function_perf.log]"
+        exit 1
+}
+
+if [ $# != 1 ] ; then
+        usage
+        exit 1;
+fi
+
+output=$1
+
+
 declare project_path="$(
         cd "$(dirname "$1")/../.."
         pwd
 )"
 echo $project_path
+export SLEDGE_SANDBOX_PERF_LOG=$project_path/runtime/tests/$output
 cd ../bin
-LD_LIBRARY_PATH="$(pwd):$LD_LIBRARY_PATH" ./sledgert ../tests/test_fibonacci.json
-#LD_LIBRARY_PATH="$(pwd):$LD_LIBRARY_PATH" ./sledgert ../tests/my_fibonacci.json
+#LD_LIBRARY_PATH="$(pwd):$LD_LIBRARY_PATH" ./sledgert ../tests/test_fibonacci.json
+#LD_LIBRARY_PATH="$(pwd):$LD_LIBRARY_PATH" ./sledgert ../tests/test_big_fibonacci.json
+LD_LIBRARY_PATH="$(pwd):$LD_LIBRARY_PATH" ./sledgert ../tests/my_fibonacci.json
 #LD_LIBRARY_PATH="$(pwd):$LD_LIBRARY_PATH" ./sledgert ../tests/test_sodresize.json
 #LD_LIBRARY_PATH="$(pwd):$LD_LIBRARY_PATH" ./sledgert ../tests/my_sodresize.json
--- runtime/tests/test.sh
+++ runtime/tests/test.sh
@@ -1,2 +1,16 @@
-hey -c 50 -z 2m -disable-keepalive -m GET -d 5 "http://127.0.0.1:10000" > output.txt
-#hey -c 50 -n 100 -disable-keepalive -m GET -d 6 "http://127.0.0.1:10000" > output.txt
+function usage {
+        echo "$0 [output file, chain_function_output.txt or single_function_output.txt] [concurrency] [duration(m)]"
+        exit 1
+}
+
+if [ $# != 3 ] ; then
+        usage
+        exit 1;
+fi
+
+output=$1
+concurrency=$2
+duration=$3
+
+hey -c $concurrency -z $durationm -disable-keepalive -m GET -d 5 "http://127.0.0.1:10000" > $output
+#hey -c 50 -n 100 -disable-keepalive -m GET -d 6 "http://127.0.0.1:10000" > $output
