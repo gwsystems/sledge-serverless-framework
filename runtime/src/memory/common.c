45792bd32be8ab1b6a1994a6b69a9768a02a6684
Sean McBride <spmcbride1201@gmail.com>
2021-09-24 17:16:45 -0400

    chore: Remove unclear FIXME

--- runtime/src/memory/common.c
+++ runtime/src/memory/common.c
@@ -11,7 +11,6 @@ initialize_region(uint32_t offset, uint32_t data_count, char *data)
 	assert(local_sandbox_context_cache.memory.size >= data_count);
 	assert(offset < local_sandbox_context_cache.memory.size - data_count);
 
-	/* FIXME: Hack around segmented and unsegmented access Issue #104 */
 	memcpy(get_memory_ptr_for_runtime(offset, data_count), data, data_count);
 }
 
