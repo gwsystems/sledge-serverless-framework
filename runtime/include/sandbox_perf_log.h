886bdbb97085a394f21cad6748fbb2b54fd1f520
Sean McBride <spmcbride1201@gmail.com>
2021-11-23 10:30:52 -0500

    fix: Add interrupted perf log header

--- runtime/include/sandbox_perf_log.h
+++ runtime/include/sandbox_perf_log.h
@@ -14,7 +14,7 @@ sandbox_perf_log_print_header()
 {
 	if (sandbox_perf_log == NULL) { perror("sandbox perf log"); }
 	fprintf(sandbox_perf_log,
-	        "id,module,port,state,deadline,actual,queued,uninitialized,allocated,initialized,runnable,preempted,"
+	        "id,module,port,state,deadline,actual,queued,uninitialized,allocated,initialized,runnable,interrupted,preempted,"
 	        "running_sys,running_user,asleep,returned,complete,error,proc_MHz,memory\n");
 }
 
