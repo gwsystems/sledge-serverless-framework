25493641b37652ea20da84d740e3101b6da5df9a
Sean McBride <spmcbride1201@gmail.com>
2021-12-01 11:57:47 -0500

    fix: Add missing type

--- runtime/include/wasm_types.h
+++ runtime/include/wasm_types.h
@@ -5,5 +5,6 @@
 /* FIXME: per-module configuration? Issue #101 */
 #define WASM_PAGE_SIZE            (1024 * 64) /* 64KB */
 #define WASM_MEMORY_PAGES_INITIAL (1 << 8)    /* 256 Pages ~16MB */
+#define WASM_MEMORY_PAGES_MAX     (1 << 16)   /* 32,768 Pages ~4GB */
 
 #define WASM_STACK_SIZE (1 << 19) /* 512KB */
