01bca00be4a7749b9538bcb73c0ba9c7ffc99bf3
Sean McBride <spmcbride1201@gmail.com>
2021-12-04 17:46:08 +0000

    build: Makefile cleanup

--- runtime/tests/fibonacci/main.c
+++ runtime/tests/fibonacci/main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-// #include "get_time.h"
-unsigned long int
-fib(unsigned long int n)
+#include <stdint.h>
+
+uint32_t
+fib(uint32_t n)
 {
 	if (n <= 1) return n;
 	return fib(n - 1) + fib(n - 2);
@@ -10,13 +11,10 @@ fib(unsigned long int n)
 int
 main(int argc, char **argv)
 {
-	unsigned long n = 0, r;
-	scanf("%lu", &n);
-	//	unsigned long long st = get_time(), en;
-	r = fib(n);
-	//	en = get_time();
-	printf("%lu\n", r);
+	uint32_t n = 0;
+	scanf("%u", &n);
 
-	//	print_time(st, en);
+	uint32_t result = fib(n);
+	printf("%u\n", result);
 	return 0;
 }
