35c6536c168a5a63f437ab181c67ef25359e17a8
Sean McBride <spmcbride1201@gmail.com>
2022-01-18 12:52:44 -0500

    chore: format nit

--- runtime/include/wasm_types.h
+++ runtime/include/wasm_types.h
@@ -6,4 +6,3 @@
 #define WASM_MEMORY_PAGES_INITIAL (1 << 8)    /* 256 Pages ~16MB */
 #define WASM_MEMORY_PAGES_MAX     (1 << 16)   /* 32,768 Pages ~4GB */
 #define WASM_STACK_SIZE           (1 << 19)   /* 512KB */
-
