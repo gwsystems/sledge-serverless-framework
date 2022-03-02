bee3df2801639f3b4da8327ac9157b2a089ed38c
Sean McBride <spmcbride1201@gmail.com>
2022-03-02 12:38:42 -0500

    chore: Update install_deb

--- install_deb.sh
+++ install_deb.sh
@@ -6,19 +6,47 @@ SHELLCHECK_URL=https://github.com/koalaman/shellcheck/releases/download/v0.7.1/s
 SHFMT_URL=https://github.com/mvdan/sh/releases/download/v3.2.4/shfmt_v3.2.4_linux_amd64
 WASI_SDK_URL=https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-12/wasi-sdk_12.0_amd64.deb
 
-apt-get update && apt-get install -y --no-install-recommends \
+sudo apt-get update && apt-get install -y --no-install-recommends \
+	apt-utils \
+	man-db \
+	&& yes | unminimize
+
+sudo apt-get update && apt-get install -y --no-install-recommends \
+	automake \
 	bc \
+	bsdmainutils \
+	build-essential \
+	binutils-dev \
+	ca-certificates \
+	cmake \
+	curl \
 	fonts-dejavu \
 	fonts-cascadia-code \
 	fonts-roboto \
+	gdb \
+	git \
+	gpg-agent \
 	gnuplot \
+	httpie \
 	imagemagick \
 	jq \
+	less \
+	libssl-dev \
+	libtinfo5 \
+	libtool \
 	libz3-4 \
+	lsb-release \
 	make \
 	netpbm \
+	openssh-client \
 	pango1.0-tools \
-	wamerican
+	pkg-config \
+	software-properties-common \
+	strace \
+	valgrind \
+	wabt \
+	wamerican \
+	wget
 
 wget $HEY_URL -O hey && chmod +x hey && sudo mv hey /usr/bin/hey
 
@@ -26,11 +54,12 @@ wget $SHFMT_URL -O shfmt && chmod +x shfmt && sudo mv shfmt /usr/local/bin/shfmt
 
 wget $SHELLCHECK_URL -O shellcheck && chmod +x shellcheck && sudo mv shellcheck /usr/local/bin/shellcheck
 
-./install_llvm.sh $LLVM_VERSION
+sudo ./install_llvm.sh $LLVM_VERSION
 
-curl -sS -L -O $WASI_SDK_URL && dpkg -i wasi-sdk_12.0_amd64.deb && rm -f wasi-sdk_12.0_amd64.deb
+curl -sS -L -O $WASI_SDK_URL && sudo dpkg -i wasi-sdk_12.0_amd64.deb && rm -f wasi-sdk_12.0_amd64.deb
 
- curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- -y
+echo "export WASI_SDK_PATH=/opt/wasi-sdk" >> ~/.bashrc
 
-echo "Add WASI_SDK_PATH to your bashrc and resource!"
-echo "Example: export WASI_SDK_PATH=/opt/wasi-sdk"
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain stable --component rustfmt --target wasm32-wasi -y
+
+echo "Run 'source ~/.bashrc'"
