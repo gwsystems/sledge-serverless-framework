a0474d34f2d29084d4fcc6a049491ddfa36ad695
Sean McBride <spmcbride1201@gmail.com>
2022-03-23 17:19:08 -0400

    chore: Remove zombie macros

--- runtime/include/wasm_types.h
+++ runtime/include/wasm_types.h
@@ -1,8 +1,4 @@
 #pragma once
 
-#include <stdint.h>
-
-#define WASM_PAGE_SIZE            (1024 * 64) /* 64KB */
-#define WASM_MEMORY_PAGES_INITIAL (1 << 8)    /* 256 Pages ~16MB */
-#define WASM_MEMORY_PAGES_MAX     (1 << 16)   /* 32,768 Pages ~4GB */
-#define WASM_STACK_SIZE           (1 << 19)   /* 512KB */
+#define WASM_PAGE_SIZE  (1024 * 64) /* 64KB */
+#define WASM_STACK_SIZE (1 << 19)   /* 512KB */
