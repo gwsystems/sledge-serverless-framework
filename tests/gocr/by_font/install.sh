52c5cfa589455b8a3c69481be97fad9902565d2a
emil <emil916@gmail.com>
2022-07-11 22:22:15 -0400

    cleanup: generalize redundant files in the experiments: - remove individual gitignore, have single gitignore in parent (tests) - remove individual env files, have single copies in 'common' dir - remove individual install.sh, have install_tools.sh (in bash libs)

--- tests/gocr/by_font/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Installs the deps needed for run.sh
-if [ "$(whoami)" == "root" ]; then
-	apt-get install netpbm pango1.0-tools wamerican fonts-roboto fonts-cascadia-code fonts-dejavu
-else
-	sudo apt-get install netpbm pango1.0-tools wamerican fonts-roboto fonts-cascadia-code fonts-dejavu
-fi
