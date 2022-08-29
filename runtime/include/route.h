e954fc1bef724b31953fccdc9c161b40c04098ba
Sean McBride <spmcbride1201@gmail.com>
2022-08-29 13:44:45 -0400

    chore: format nit

--- runtime/include/route.h
+++ runtime/include/route.h
@@ -9,12 +9,12 @@
 
 /* Assumption: entrypoint is always _start. This should be enhanced later */
 struct route {
-	char	           *route;
+	char                   *route;
 	struct http_route_total metrics;
 	struct module          *module;
 	/* HTTP State */
 	uint32_t               relative_deadline_us;
 	uint64_t               relative_deadline; /* cycles */
-	char	          *response_content_type;
+	char                  *response_content_type;
 	struct admissions_info admissions_info;
 };
