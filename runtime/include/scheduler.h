183f53faaeed8709cefdacd551a2e46f62aed673
adroitx <abhi.sharma1177@gmail.com>
2023-01-31 13:02:09 -0800

    debug enabled

--- runtime/include/scheduler.h
+++ runtime/include/scheduler.h
@@ -409,10 +409,7 @@ scheduler_idle_loop()
 		local_cleanup_queue_free();
 
 		/* Improve the performance of spin-wait loops (works only if preemptions enabled) */
-		if (runtime_worker_spinloop_pause_enabled) {
-		    debuglog("Inside scheduler idle loop, pause enabled")
-		    pause();
-		}
+		if (runtime_worker_spinloop_pause_enabled) pause();
 	}
 }
 
