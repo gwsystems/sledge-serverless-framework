21595abf1f0fe3106ac8fa41b85b9b769d4fad8c
Xiaosu Lyu <lyuxiaosu@gwu.edu>
2024-06-17 14:49:31 -0600

    update curl.sh and measure_old_sledge_cost.sh of folder ori_sledge_tests

--- runtime/tests/ori_sledge_tests/curl.sh
+++ runtime/tests/ori_sledge_tests/curl.sh
@@ -1,2 +1,3 @@
 #curl -I -w "Total time: %{time_total}s\n" http://10.10.1.1:31850/empty
 curl -w "Total time: %{time_total}s\n" http://10.10.1.1:31850/empty http://10.10.1.1:31850/empty http://10.10.1.1:31850/empty http://10.10.1.1:31850/empty
+#curl -w "Total time: %{time_total}s\n" http://10.10.1.1:31850/empty http://10.10.1.1:31850/empty 
--- runtime/tests/ori_sledge_tests/measure_old_sledge_cost.sh
+++ runtime/tests/ori_sledge_tests/measure_old_sledge_cost.sh
@@ -15,13 +15,14 @@ remote_ip="10.10.1.1"
 repeat_count=$1
 > old_sledge.log
 
-path="/my_mount/ori_sledge/sledge-serverless-framework/runtime/tests"
+path="/my_mount/old_sledge/sledge-serverless-framework/runtime/tests"
 for(( i=0;i<$repeat_count;i++ )) do
 	echo "i is $i"
 	echo "start server..."
         ssh -o stricthostkeychecking=no -i ./id_rsa xiaosuGW@$remote_ip "sudo $path/start.sh > 1.txt 2>&1 &"
         sleep 1 
 	echo "start client..."
-        ./curl.sh >> old_sledge.log
+        #./curl.sh >> old_sledge.log
+        ./curl.sh 
         ssh -o stricthostkeychecking=no -i ./id_rsa xiaosuGW@$remote_ip  "sudo $path/kill_sledge.sh"
 done
