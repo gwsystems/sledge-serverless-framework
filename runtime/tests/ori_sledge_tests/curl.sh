ea6f45135395c2c4b15427955dc159f7884e596e
Xiaosu Lyu <lyuxiaosu@gwu.edu>
2024-07-16 23:47:06 -0600

    update scripts

--- runtime/tests/ori_sledge_tests/curl.sh
+++ runtime/tests/ori_sledge_tests/curl.sh
@@ -1,6 +1,6 @@
 #curl -I -w "Total time: %{time_total}s\n" http://10.10.1.1:31850/empty
 #curl -w "Total time: %{time_total}s\n" http://10.10.1.1:31850/empty http://10.10.1.1:31850/empty http://10.10.1.1:31850/empty http://10.10.1.1:31850/empty
-curl -w "Total time: %{time_total}s TCP Connect time: %{time_connect}s\n" -o /dev/null -s http://10.10.1.1:31850/empty http://10.10.1.1:31850/empty 
+#curl -I -H "Connection: keep-alive" -w "Total time: %{time_total}s TCP Connect time: %{time_connect}s\n" -o /dev/null -s http://10.10.1.1:31850/empty http://10.10.1.1:31850/empty 
 
 
-#curl -H 'Expect:' -H "Content-Type: image/bmp" --data-binary "@frog5.bmp" "10.10.1.1:31850/classify"
+curl -H 'Expect:' -H "Content-Type: image/bmp" --data-binary "@frog5.bmp" "10.10.1.1:31850/classify"
--- runtime/tests/ori_sledge_tests/parse_cost.py
+++ runtime/tests/ori_sledge_tests/parse_cost.py
@@ -15,6 +15,7 @@ except FileNotFoundError:
 
 total_cold_time = 0
 total_tcp_conn = 0
+total_tcp_conn2 = 0
 total_warm_time = 0
 count = 0
 
@@ -22,21 +23,25 @@ for i in range(0, len(lines), 2):
     time_str_1 = lines[i].split(" ")[2].strip()
     tcp_cost_str = lines[i].split(" ")[6].strip()
     time_str_2 = lines[i+1].split(" ")[2].strip()
+    tcp_cost_str2 = lines[i+1].split(" ")[6].strip()
     time_value_1 = float(time_str_1[:-1])          
     time_value_2 = float(time_str_2[:-1])    
     tcp_cost = float(tcp_cost_str[:-1])
+    tcp_cost2 = float(tcp_cost_str2[:-1])
 
     print("cold:", time_value_1, " warm:", time_value_2)
     total_cold_time += time_value_1
     total_warm_time += time_value_2      
     total_tcp_conn += tcp_cost      
+    total_tcp_conn2 += tcp_cost2
     count += 1                             
 
 average_cold_time = total_cold_time / count
 average_warm_time = total_warm_time / count
 average_tcp_conn = total_tcp_conn / count
+average_tcp_conn2 = total_tcp_conn2 / count
 
 print("Average cold time:", average_cold_time, "s")
 print("Average warm time:", average_warm_time, "s")
 print("Average tcp conn:", average_tcp_conn, "s")
-
+print("Average tcp conn2:", average_tcp_conn2, "s")
