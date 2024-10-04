a82a971d4c7910fe79a318d4f7d21bdc4d4dfc24
Xiaosu Lyu <lyuxiaosu@gwu.edu>
2024-10-04 16:08:37 -0600

    upload monitor_mem.sh kill_perf.sh run_perf.sh

---
+++ runtime/tests/run_perf.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function usage {
+        echo "$0 [output]"
+        exit 1
+}
+
+if [ $# != 1 ] ; then
+        usage
+        exit 1;
+fi
+
+pid=`ps -ef|grep  "sledgert"|grep -v grep |awk '{print $2}'`
+output=$1
+
+cpu=$(lscpu | awk '/^Core\(s\) per socket:/ {cores=$4} /^Socket\(s\):/ {sockets=$2} END {print cores * sockets}')
+last_core=$((cpu - 1)) #get the last core
+
+taskset -c $last_core sudo perf stat -B -e context-switches,cache-references,cache-misses,cycles,instructions,page-faults -p $pid -o $output 2>&1 &
+
